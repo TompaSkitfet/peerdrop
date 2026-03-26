4b1e948948e8f0763a675f01dc02c58abbf76dda
Tommy Andersson <tommyandersson1992@gmail.com>
2026-03-26 15:57:55 +0100

    lite ws

--- desktop/app.go
+++ desktop/app.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/TompaSkitfet/peerdrop/rtc"
+	"github.com/gorilla/websocket"
 )
 
 // App struct
 type App struct {
-	ctx context.Context
+	ctx     context.Context
+	manager *rtc.WebRTCManger
+	ws      *websocket.Conn
 }
 
 // NewApp creates a new App application struct
@@ -19,9 +24,31 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+	a.manager = rtc.NewManager()
+	a.connectWebSocket()
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) connectWebSocket() {
+	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/ws", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	a.ws = conn
+	go a.listenWS()
+}
+
+func (a *App) listenWS() {
+	for {
+		_, msg, err := a.ws.ReadMessage()
+		if err != nil {
+			fmt.Println("WS error: ", err)
+			return
+		}
+		a.handleSignal(msg)
+	}
+}
+
+func (a *App) handleSignal(msg []byte) {
+
 }
--- desktop/rtc/manager.go
+++ desktop/rtc/manager.go
@@ -1,4 +1,51 @@
 package rtc
 
+import (
+	"sync"
+
+	"github.com/pion/webrtc/v3"
+)
+
 type WebRTCManger struct {
+	peer *Peer
+	mu   sync.RWMutex
+	emit func(string, interface{})
 }
+
+func NewManager() *WebRTCManger {
+	return &WebRTCManger{}
+}
+
+func (m *WebRTCManger) CreatePeer(id string) error {
+	cfg := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{{
+			URLs: []string{"stun:stun.l.google.com:19302"},
+		}},
+	}
+
+	pc, err := webrtc.NewPeerConnection(cfg)
+	if err != nil {
+		return err
+	}
+
+	pc.OnICECandidate(func(c *webrtc.ICECandidate) {
+		if c == nil {
+			return
+		}
+
+		m.emit("ice-candidate", c.ToJSON())
+	})
+
+	pc.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		m.emit("connection-state", s.String())
+	})
+
+	m.peer = &Peer{
+		PC: pc,
+	}
+
+	return nil
+}
+
+func (m *WebRTCManger) SendFile(path string) error
+func (m *WebRTCManger) ClosePeer()
