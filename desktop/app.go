076296c9a857224ea5d4502f9804c633b6d4c8c3
Tommy Andersson <tommyandersson1992@gmail.com>
2026-03-27 13:21:07 +0100

    lite realtimerefact

--- desktop/app.go
+++ desktop/app.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/TompaSkitfet/peerdrop/realtime"
 	"github.com/TompaSkitfet/peerdrop/rtc"
-	"github.com/gorilla/websocket"
 )
 
 // App struct
 type App struct {
-	ctx     context.Context
-	manager *rtc.WebRTCManger
-	ws      *websocket.Conn
+	ctx           context.Context
+	webrtcManager *rtc.WebRTCManger
+	wsManager     *realtime.WebsockerManager
 }
 
 // NewApp creates a new App application struct
@@ -24,31 +23,6 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-	a.manager = rtc.NewManager()
-	a.connectWebSocket()
-}
-
-func (a *App) connectWebSocket() {
-	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/ws", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	a.ws = conn
-	go a.listenWS()
-}
-
-func (a *App) listenWS() {
-	for {
-		_, msg, err := a.ws.ReadMessage()
-		if err != nil {
-			fmt.Println("WS error: ", err)
-			return
-		}
-		a.handleSignal(msg)
-	}
-}
-
-func (a *App) handleSignal(msg []byte) {
-
+	a.webrtcManager = rtc.NewManager()
+	a.wsManager.ConnectWebSocket()
 }
